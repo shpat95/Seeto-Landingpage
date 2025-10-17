2b332937e9776cd1296fb461965f2f807a8ec805
shpat95 <shpath2@hotmail.com>
2025-10-17 20:11:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Seeto-Landingpage/',   // 👈 wichtig für GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
